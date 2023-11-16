367e46b7abc45d8fcce6195809527d5c1077264d
Karapostolis <akisgr4321@gmail.com>
2023-11-16 14:31:47 +0200

    Entered the score Entered Images for player,enemy,bullet Entered collision Code improvements

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 import sys 
 import time
+# Import random for random numbers
+import random
 import pygame_menu as pm 
+from os import path
 from pygame.locals import (
     RLEACCEL,
     K_UP,
@@ -46,25 +49,58 @@ BRIGHT_GREEN = (51,204,51)
 # Define a Player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
 class Player(pygame.sprite.Sprite):
+    global all_sprites
+
     def __init__(self):
         # Call the parent class (Sprite) constructor
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
+
+        self.surf = pygame.image.load("Assets/Spaceship.png").convert_alpha()
+        #self.surf.set_colorkey((0, 0, 0), RLEACCEL)
+        #self.surf = pygame.Surface((75, 25))
         # Set the background color
-        self.surf.fill((255, 255, 255))
+        #self.surf.fill((255, 255, 255))
         # Fetch the rectangle object that has the dimensions of surf
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT - 100))
+        # The x value of center
+        self.rect.centerx = SCREEN_WIDTH/2
+        # The shoot delay 
+        self.shoot_delay = 250
+        # The last shot
+        self.last_shot = pygame.time.get_ticks()
+        # Players lives
+        self.lives = 3
+
+
+    # Fuction that handles shooting 
+    def shoot(self):
+        ## to tell the bullet where to spawn
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+             self.last_shot = now
+             bullet = Bullet(self.rect.centerx, self.rect.top)
+             all_sprites.add(bullet)
+             bullets.add(bullet)
+             #shooting_sound.play()
+
+
+
 
     # Move the sprite based on user keypresses
-    def update(self, pressed_keys):
+    def update(self):
+        # Get all the keys currently pressed
+        pressed_keys = pygame.key.get_pressed()
+
         if pressed_keys[K_UP]:
-             self.rect.move_ip(0, -5)
+             self.rect.move_ip(0, -10)
         if pressed_keys[K_DOWN]:
-             self.rect.move_ip(0, 5)
+             self.rect.move_ip(0, 10)
         if pressed_keys[K_LEFT]:
-             self.rect.move_ip(-5, 0)
+             self.rect.move_ip(-10, 0)
         if pressed_keys[K_RIGHT]:
-             self.rect.move_ip(5, 0)
+             self.rect.move_ip(10, 0)
+        if pressed_keys[pygame.K_SPACE]:
+             self.shoot()
 
         # Keep player on the screen
         if self.rect.left < 0:
@@ -76,7 +112,57 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= SCREEN_HEIGHT:
              self.rect.bottom = SCREEN_HEIGHT
 
+# Define the enemy object by extending pygame.sprite.Sprite
+# The surface you draw on the screen is now an attribute of 'enemy'
+class Enemy(pygame.sprite.Sprite):
+  
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("Assets/Enemy.png").convert_alpha()
+        #self.surf.set_colorkey((0, 0, 0), RLEACCEL)
 
+        #self.surf = pygame.Surface((20, 10))
+        #self.surf.fill((255, 255, 255))
+        # Makes the enemy to be drawn at the top of the screen 
+        # Between random(x = 0 - screem_width) and y = -20
+        self.rect = self.surf.get_rect(
+            center=(
+               random.randint(0, SCREEN_WIDTH),
+               -5,
+            )
+        )
+        # The speed at which the enemy will be moving
+        self.speed = 7
+
+    # Move the sprite based on speed
+    # Remove the sprite when it passes the left edge of the screen
+    def update(self):
+        global score        
+        self.rect.move_ip(0, self.speed)
+        if self.rect.bottom < 0:
+            self.kill()
+        if pygame.sprite.spritecollideany(self, bullets):
+               score = score + 20               
+               self.kill()
+
+## defines the sprite for bullets
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.surf = pygame.image.load('Assets/Laser.png').convert()
+        self.surf.set_colorkey(BLACK)
+        self.rect = self.surf.get_rect()
+        ## place the bullet according to the current position of the player
+        self.rect.bottom = y 
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        """should spawn right in front of the player"""
+        self.rect.y += self.speedy
+        ## kill the sprite after it moves over the top border
+        if self.rect.bottom < 0:
+            self.kill()
 
 # Function that changes the difficulty of the game
 def Game_Difficulty(dif_list,dif):
@@ -122,29 +208,59 @@ def unpause():
 
 # Fuction to Pause the Game
 def paused():
-    global pause
-    largeText = pygame.font.SysFont("comicsansms",115)
-    TextSurf, TextRect = text_objects("Paused", largeText)
-    TextRect.center = ((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
-    screen.blit(TextSurf, TextRect)
+     global pause
+     
+     while pause:
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
 
-    
+          # Makes the text
+          largeText = pygame.font.SysFont("comicsansms",115)
+          textSurf, textRect = text_objects("Paused", largeText)
+          textRect.center = ( (SCREEN_WIDTH/2), (SCREEN_HEIGHT/2) )
+          screen.blit(textSurf,textRect)
 
-    while pause:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-                
-        #screen.fill(WHITE)
+          # Makes the buttons
+          button("Resume",150,450,100,50,GREEN,BRIGHT_GREEN,unpause)
+          button("Quit",450,450,100,50,RED,BRIGHT_RED,quit_Game)
+
+          # Displays the buttons and the text
+          pygame.display.update()
+          clock.tick(15)  
         
+# Fuction that restarts the game after he/she loses
+def restart():
+     global pause
+     global score
+     score = 0
+     while pause:
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
 
-        button("Resume",150,450,100,50,GREEN,BRIGHT_GREEN,unpause)
-        button("Quit",450,450,100,50,RED,BRIGHT_RED,quit_Game)
+          #screen.fill(WHITE)
 
-        pygame.display.update()
-        clock.tick(15)  
-   
+          smallText = pygame.font.SysFont("comicsansms",115)
+          textSurf, textRect = text_objects("You Lost",smallText)
+          textRect.center = ( (SCREEN_WIDTH/2), (SCREEN_HEIGHT/2) )
+          screen.blit(textSurf,textRect)
+
+          button("Restart",150,450,100,50,GREEN,BRIGHT_GREEN,playFun)
+          button("Quit",450,450,100,50,RED,BRIGHT_RED,quit_Game)
+
+          pygame.display.update()
+          clock.tick(15) 
+
+# Fuction that displays the Score  
+def Score():
+     smallText = pygame.font.Font("freesansbold.ttf",20)
+     textSurf, textRect = text_objects(f"Score: {score}",smallText)
+     textRect.center = ( SCREEN_WIDTH-70, SCREEN_HEIGHT-30 )
+     screen.blit(textSurf,textRect)
+     pygame.display.update()
 
 # Fuctoin of the main game
 def playFun():
@@ -157,12 +273,34 @@ def playFun():
      # Enter the global variable pause so we can pause the game
      global pause
 
+     # Create a custom event for adding a new enemy
+     ADDENEMY = pygame.USEREVENT + 1
+     pygame.time.set_timer(ADDENEMY, 250)
+
      # Instantiate player. 
      player = Player()
+
+     # Create groups to hold enemy sprites and all sprites
+     # - enemies is used for collision detection and position updates
+     # - all_sprites is used for rendering
+     global enemies
+     enemies = pygame.sprite.Group()
+
+     # Create groups to hold bullets sprites and all sprites
+     # - bullets is used for the shooting function
+     # - all_sprites is used for rendering
+     global bullets
+     bullets = pygame.sprite.Group()
+
+     # Make a group for all sprites
+     global all_sprites
+     all_sprites = pygame.sprite.Group()
+     all_sprites.add(player)
 
      # Initialize the running variable
      running = True
      while running:
+          Score()
           for event in pygame.event.get():
                if event.type == KEYDOWN:
                     # If player presses Escape
@@ -170,26 +308,43 @@ def playFun():
                          pause = True
                          # Pause the game
                          paused()
+                    
+
                elif event.type == pygame.QUIT:
                     quit_Game()
-
+               # Add a new enemy?
+               elif event.type == ADDENEMY:
+                    # Create the new enemy and add it to sprite groups
+                    new_enemy = Enemy()
+                    enemies.add(new_enemy)
+                    all_sprites.add(new_enemy)
                
-          #score +=1
-          
-          # Get all the keys currently pressed
-          pressed_keys = pygame.key.get_pressed()
-
-          # Update the player sprite based on user keypresses
-          player.update(pressed_keys)
+                    
+          # Update all sprites
+          all_sprites.update()
           
           # Fills the screen with the color black
-          game_screen.fill((0, 0, 0))
+          game_screen.fill((255, 255,255))
+          
+          # Draw all sprites
+          for entity in all_sprites:
+               screen.blit(entity.surf, entity.rect)
+          
 
-          # Draw the player on the screen
-          screen.blit(player.surf, player.rect)
+          # Check if any enemies have collided with the player
+          if pygame.sprite.spritecollideany(player, enemies):
+               # If so, then remove the player and stop the loop
+               player.kill()
+               running = False
+               pause = True
+               restart()
+
 
           # Flips the screen
           pygame.display.flip()
+
+          # Ensure program maintains a rate of 30 frames per second
+          clock.tick(60)
 
 # Main Function (That is also the main menu at the start of the game)
 def menuFun(): 
